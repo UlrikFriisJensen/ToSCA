163b311052a4eb7c2308d04dd4c44de28f338656
UlrikFriis-Jensen <lgb543@alumni.ku.dk>
2024-09-23 12:18:33 +0200

    First tests of training code

--- modules/net.py
+++ modules/net.py
@@ -4,51 +4,127 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import Sequential as pyg_Sequential, GATv2Conv, global_mean_pool, global_max_pool, global_add_pool
+from torch_geometric.nn.aggr import MLPAggregation, SetTransformerAggregation, PowerMeanAggregation
 from torch.nn import Sequential, Conv1d
+
+#%% Custom layers
+
+class GatedConv1d(nn.Module):
+    def __init__(self, input_channels, output_channels,
+                 kernel_size, stride, padding=0, dilation=1, activation=None):
+        super(GatedConv1d, self).__init__()
+
+        self.activation = activation
+        self.sigmoid = nn.Sigmoid()
+
+        self.h = Conv1d(input_channels, output_channels, kernel_size,
+                           stride, padding, dilation)
+        self.g = Conv1d(input_channels, output_channels, kernel_size,
+                           stride, padding, dilation)
+
+    def forward(self, x):
+        if self.activation is None:
+            h = self.h(x)
+        else:
+            h = self.activation(self.h(x))
+        g = self.sigmoid(self.g(x))
+
+        return h * g
 
 #%% Model
 
-class SCVAE(torch.nn.Module):
-    def __init__(self, ):
+class SCVAE(nn.Module):
+    def __init__(self, latent_dim=128, out_dim=50, gnn_dim=128, gnn_heads=1, gnn_edge_dim=1, scattering_channels=2, scattering_dim=256, scattering_kernel_size=7, scattering_stride=3, scattering_padding=1, decoder_hidden_dim=256):
         super(SCVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.out_dim = out_dim
+        self.gnn_dim = gnn_dim
+        self.gnn_heads = gnn_heads
+        self.gnn_edge_dim = gnn_edge_dim
+        self.scattering_channels = scattering_channels
+        self.scattering_dim = scattering_dim
+        self.scattering_kernel_size = scattering_kernel_size
+        self.scattering_stride = scattering_stride
+        self.scattering_padding = scattering_padding
+        self.decoder_hidden_dim = decoder_hidden_dim
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        
         self.graph_encoder_local = pyg_Sequential('x, edge_index, edge_attr', [
-            (GATv2Conv(128, 128, heads=8, concat=True), 'x, edge_index, edge_attr -> x'),
+            (GATv2Conv(7, self.gnn_dim, heads=self.gnn_heads, concat=True, edge_dim=self.gnn_edge_dim), 'x, edge_index, edge_attr -> x'),
             nn.ELU(),
-            (GATv2Conv(128*8, 128, heads=8, concat=True), 'x, edge_index, edge_attr -> x'),
+            (GATv2Conv(self.gnn_dim*self.gnn_heads, self.gnn_dim, heads=self.gnn_heads, concat=True, edge_dim=self.gnn_edge_dim), 'x, edge_index, edge_attr -> x'),
             nn.ELU(),
         ])
         
         self.graph_encoder_global = pyg_Sequential('x, edge_index, edge_attr', [
-            (GATv2Conv(128, 128, heads=8, concat=True), 'x, edge_index, edge_attr -> x'),
+            (GATv2Conv(self.gnn_dim*self.gnn_heads, self.gnn_dim, heads=self.gnn_heads, concat=True, edge_dim=self.gnn_edge_dim), 'x, edge_index, edge_attr -> x'),
             nn.ELU(),
-            (GATv2Conv(128*8, 128, heads=8, concat=True), 'x, edge_index, edge_attr -> x'),
+            (GATv2Conv(self.gnn_dim*self.gnn_heads, self.gnn_dim, heads=self.gnn_heads, concat=True, edge_dim=self.gnn_edge_dim), 'x, edge_index, edge_attr -> x'),
             nn.ELU(),
-            (GATv2Conv(128*8, 128, heads=8, concat=True), 'x, edge_index, edge_attr -> x'),
+            (GATv2Conv(self.gnn_dim*self.gnn_heads, self.gnn_dim, heads=self.gnn_heads, concat=True, edge_dim=self.gnn_edge_dim), 'x, edge_index, edge_attr -> x'),
             nn.ELU(),
-            (GATv2Conv(128*8, 128, heads=8, concat=True), 'x, edge_index, edge_attr -> x'),
+            (GATv2Conv(self.gnn_dim*self.gnn_heads, self.gnn_dim, heads=self.gnn_heads, concat=True, edge_dim=self.gnn_edge_dim), 'x, edge_index, edge_attr -> x'),
             nn.ELU(),
         ])
         
-        self.scattering_encoder = Sequential(
-            Conv1d(2, 128, 7, padding=1),
+        self.linear_graph_encoder = Sequential(
+            nn.Linear(self.latent_dim*self.gnn_heads*6, self.latent_dim*4),
             nn.ELU(),
-            Conv1d(128, 128, 7, padding=1),
+            nn.Linear(self.latent_dim*4, self.latent_dim*2),
+        )
+        
+        # self.scattering_encoder = Sequential(
+            
+        # )
+        
+        # self.local_aggregator = MLPAggregation(
+        #     in_channels = self.gnn_dim*self.gnn_heads, 
+        #     out_channels = self.latent_dim*2, 
+        #     max_num_elements = self.out_dim,
+        #     num_layers = 1
+        #     ).to(self.device)
+        
+        self.local_aggregator = PowerMeanAggregation(
+            p=1.0,
+            learn=True,
+            channels=1,
+        )
+        
+        # self.global_aggregator = MLPAggregation(
+        #     in_channels = self.gnn_dim*self.gnn_heads, 
+        #     out_channels = self.latent_dim*2, 
+        #     max_num_elements = self.out_dim,
+        #     num_layers = 1
+        #     ).to(self.device)
+        
+        self.global_aggregator = PowerMeanAggregation(
+            p=1.0,
+            learn=True,
+            channels=1,
+        )
+        
+        self.shared_decoder = Sequential(
+            nn.Linear(self.latent_dim, self.decoder_hidden_dim),
             nn.ELU(),
-            nn.MaxPool1d(5, stride=3, padding=1),
-            Conv1d(128, 256, 7, padding=1),
-            nn.ELU(),
-            Conv1d(256, 256, 7, padding=1),
+            nn.Linear(self.decoder_hidden_dim, self.decoder_hidden_dim),
             nn.ELU(),
         )
         
-        self.combined_encoder = Sequential(
-            nn.Linear(128*2 + 256*2, 128),
-            nn.ELU(),
-            nn.Linear(128, 64),
-            nn.ELU(),
-            nn.Linear(64, 32),
+        self.cell_parameter_decoder = Sequential(
+            nn.Linear(self.decoder_hidden_dim, 6),
         )
         
+        self.cell_position_decoder = Sequential(
+            nn.Linear(self.decoder_hidden_dim, self.decoder_hidden_dim),
+            nn.ELU(),
+            nn.Linear(self.decoder_hidden_dim, self.out_dim*3),
+        )
+        
+        self.cell_atom_decoder = Sequential(
+            nn.Linear(self.decoder_hidden_dim, self.decoder_hidden_dim),
+            nn.ELU(),
+            nn.Linear(self.decoder_hidden_dim, self.out_dim*118),
+        )
         
 
     def encode(self, x, edge_index, edge_attr, batch):
@@ -57,21 +133,45 @@ class SCVAE(torch.nn.Module):
         z_global = self.graph_encoder_global(z_local, edge_index, edge_attr)
         
         z_local = torch.cat([global_mean_pool(z_local, batch), global_max_pool(z_local, batch), global_add_pool(z_local, batch)], dim=1)
+        # z_local = self.local_aggregator(z_local, batch, dim_size=self.latent_dim*2)
+        
+        z_global = torch.cat([global_mean_pool(z_global, batch), global_max_pool(z_global, batch), global_add_pool(z_global, batch)], dim=1)
+        # z_global = self.global_aggregator(z_global, batch, dim_size=self.latent_dim*2)
         
         z_graph = torch.cat((z_local, z_global), dim=1)
         
-        return
+        z_graph = self.linear_graph_encoder(z_graph)
+        
+        mean, log_std = z_graph.chunk(2, dim=-1)
+        
+        return mean, log_std
+    
+    # def prior(self, scattering):
+    #     z_scattering = self.scattering_encoder(scattering)
     
     def reparameterize(self, mean, log_std):
         std = F.softplus(log_std) #torch.exp(0.5 * log_std)
         eps = torch.randn_like(std)
         return mean + eps * std
         
-    def decode(self):
-        return
+    def decode(self, z):
+        
+        z = self.shared_decoder(z)
+        
+        cell_parameters = self.cell_parameter_decoder(z)
+        cell_parameters = cell_parameters.view(-1, 6)
+        
+        cell_positions = self.cell_position_decoder(z)
+        cell_positions = cell_positions.view(-1, self.out_dim, 3)
+        
+        cell_atoms = self.cell_atom_decoder(z)
+        cell_atoms = cell_atoms.view(-1, self.out_dim, 118)
+        
+        return cell_parameters, cell_positions, cell_atoms
 
-    def forward(self, data):
-        mu, logvar = self.encoder(data)
-        z = self.reparameterize(mu, logvar)
-        return self.decoder(z), mu, logvar, self.classifier(z)
+    def forward(self, x, edge_index, edge_attr=None, batch=None):
+        mean, log_std = self.encode(x, edge_index, edge_attr, batch)
+        z = self.reparameterize(mean, log_std)
+        cell_parameters, cell_positions, cell_atoms = self.decode(z)
+        return cell_parameters, cell_positions, cell_atoms, mean, log_std
     
--- train.py
+++ train.py
@@ -8,6 +8,9 @@ from modules.net import SCVAE
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import Sequential as pyg_Sequential
 import torch
+import datetime
+import pathlib
+from tqdm.auto import tqdm
 
 #%% Suppress warnings
 warnings.filterwarnings("ignore")
@@ -25,6 +28,13 @@ if __name__ == "__main__":
         setup_json = json.load(f)
     
     #%% General setup
+    
+    # Make experiment folder
+    experiment_folder = f'{setup_json["model_root"]}/{setup_json["experiment_name"]}'
+    pathlib.Path(experiment_folder).mkdir(parents=True, exist_ok=True)
+    
+    # Record date and time
+    setup_json['experiment_start'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
     # Set random seed
     torch.manual_seed(setup_json['random_seed'])
@@ -64,14 +74,34 @@ if __name__ == "__main__":
         )
 
     # Dataloaders
-    train_loader = DataLoader(dataset.train_set, batch_size=setup_json['data']['batch_size'], shuffle=True)
-    validation_loader = DataLoader(dataset.validation_set, batch_size=setup_json['data']['batch_size'], shuffle=False)
+    train_loader = DataLoader(dataset.train_set, batch_size=setup_json['data']['batch_size'], shuffle=True, num_workers=setup_json['data']['num_workers'])
+    validation_loader = DataLoader(dataset.validation_set, batch_size=setup_json['data']['batch_size'], shuffle=False, num_workers=setup_json['data']['num_workers'])
     
     #%% Prepare training
 
-    # Instantiate model
-    model = SCVAE().to(device)
+    # Determine output dimension
+    out_dim = 0
+    if setup_json['model']['out_dim'] is None:
+        for batch in train_loader:
+            _out_dim = torch.amax(batch.y['unit_cell_n_atoms'])
+            if _out_dim > out_dim:
+                out_dim = _out_dim
+        setup_json['model']['out_dim'] = out_dim.item()
         
+    # Save setup json in model directory
+    with open(experiment_folder + '/setup_json.json', 'w') as f:
+        json.dump(setup_json, f, indent=4)
+    
+    # Instantiate model
+    model = SCVAE(
+        latent_dim=setup_json['model']['latent_dim'],
+        out_dim=setup_json['model']['out_dim'],
+    )
+    model = model.to(device)
+    
+    # Print model summary
+    print(model)
+    
     # Load checkpoint if specified
     if setup_json['start_from_checkpoint'] is not None:
         model.load_state_dict(torch.load(setup_json['checkpoint']))
@@ -79,22 +109,94 @@ if __name__ == "__main__":
     # Optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=setup_json['training']['learning_rate'])
     
-    # Loss function
-    loss_fn = torch.nn.CrossEntropyLoss()
+    # Loss functions
+    loss_fn_cell_parameters = torch.nn.MSELoss()
+    loss_fn_cell_positions = torch.nn.MSELoss()
+    loss_fn_cell_atoms = torch.nn.CrossEntropyLoss()
+    loss_fn_kld = torch.nn.KLDivLoss()
         
     #%% Train model
-
+    best_loss = np.inf
+    patience = setup_json['training']['patience']
+    patience_counter = 0
+    best_epoch = 0
     for epoch in range(setup_json['training']['epochs']):
+        # Check patience
+        if patience_counter >= patience:
+            print(f'Early stopping after {epoch} epochs')
+            break
         # Train model
-        for batch in train_loader:
+        model.train()
+        train_loss = 0
+        for batch in tqdm(train_loader, desc='Training', leave=False):
+            # Zero gradients
+            optimizer.zero_grad()
+            
+            # Forward pass
             batch = batch.to(device)
-            
-            
-            raise ValueError
-            pass
+            # print(batch.is_cuda)
+            # print(next(model.parameters()).is_cuda)
+            cell_parameters, cell_positions, cell_atoms, mean, log_std = model.forward(
+                x = torch.cat((batch.x, batch.pos_abs), dim=1), 
+                edge_index = batch.edge_index, 
+                edge_attr = batch.edge_attr, 
+                batch = batch.batch,
+            )
 
+            # Loss
+            loss_cell_parameters = loss_fn_cell_parameters(cell_parameters, batch.y['cell_params'].view(-1, 6))
+            loss_cell_positions = 0 # Placeholder
+            loss_cell_atoms = 0 # Placeholder
+            loss_kld = 0 # Placeholder
+            
+            total_loss = loss_cell_parameters + loss_cell_positions + loss_cell_atoms + loss_kld
+            
+            # Backward pass
+            total_loss.backward()
+            optimizer.step()
+            
+            # Store loss
+            train_loss += total_loss.item()
+        
+        train_loss /= len(train_loader)
+        
         # Validate model
-        pass
+        model.eval()
+        validation_loss = 0
+        for batch in tqdm(validation_loader, desc='Validation', leave=False):
+            # Forward pass
+            batch = batch.to(device)
+            cell_parameters, cell_positions, cell_atoms, mean, log_std = model.forward(
+                x = torch.cat((batch.x, batch.pos_abs), dim=1), 
+                edge_index = batch.edge_index, 
+                edge_attr = batch.edge_attr, 
+                batch = batch.batch,
+            )
 
-        # Save model
-        pass
+            # Loss
+            loss_cell_parameters = loss_fn_cell_parameters(cell_parameters, batch.y['cell_params'].view(-1, 6))
+            loss_cell_positions = 0 # Placeholder
+            loss_cell_atoms = 0 # Placeholder
+            loss_kld = 0 # Placeholder
+            
+            total_loss = loss_cell_parameters + loss_cell_positions + loss_cell_atoms + loss_kld
+            
+            # Store loss
+            validation_loss += total_loss.item()
+
+        validation_loss /= len(validation_loader)
+        
+        # Check if model improved
+        if validation_loss < best_loss:
+            patience_counter = 0
+            best_epoch = epoch
+            best_loss = validation_loss
+            torch.save(model.state_dict(), f'{experiment_folder}/best_model.pth')
+        else:
+            patience_counter += 1
+            
+        # Save latest model
+        torch.save(model.state_dict(), f'{experiment_folder}/latest_model.pth')
+        
+        # Print progress
+        print(f'Epoch: {epoch} | Train loss: {train_loss:.2e} | Validation loss: {validation_loss:.2e} | Best loss: {best_loss:.2e} (Epoch {best_epoch}) | Patience: {patience_counter}/{patience}')
