fc9bdf9ddfd49d833bee49c6a41106a0e1e15092
lgb543 <ufj@chem.ku.dk>
2024-10-22 15:09:19 +0200

    Experiments

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=weighted_loss
+#SBATCH --job-name=2d_latentspace_bigger_encoding
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=6000M
 
 #SBATCH -p gpu --gres=gpu:titanrtx:1
 
-#SBATCH --time=01:00:00
+#SBATCH --time=2-00:00:00
 
 #SBATCH -o ./slurm_outputs/scvae-%j.out #STDOUT
 
 hostname
 echo $CUDA_VISIBLE_DEVICES
 
-# python train.py --setup_json test_setup.json
+python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/weighted_loss/setup_json.json
+python test.py --test_data validation --setup_json ./models/2d_latentspace_bigger_encoding/setup_json.json
