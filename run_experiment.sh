f9e1123e431a5a1ae306198a3778ed7f2d650354
lgb543 <ufj@chem.ku.dk>
2024-10-25 20:33:00 +0200

    Experiments

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=2d_latentspace_larger_encoding
+#SBATCH --job-name=larger_encoding_low_lr
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=6000M
 
 #SBATCH -p gpu --gres=gpu:titanrtx:1
 
-#SBATCH --time=0-01:00:00
+#SBATCH --time=2-00:00:00
 
 #SBATCH -o ./slurm_outputs/scvae-%j.out #STDOUT
 
 hostname
 echo $CUDA_VISIBLE_DEVICES
 
-# python train.py --setup_json test_setup.json
+python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/2d_latentspace_larger_encoding/setup_json.json
+python test.py --test_data validation --setup_json ./models/larger_encoding_low_lr/setup_json.json
