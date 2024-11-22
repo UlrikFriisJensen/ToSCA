09d9131f26da6638dc969c458ebb55a231daef84
lgb543 <ufj@chem.ku.dk>
2024-11-22 14:18:57 +0100

    experiment setup

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=unit_cell_graph_test
+#SBATCH --job-name=unit_cell_graph_only_structure
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=8000M
 
@@ -14,4 +14,4 @@ echo $CUDA_VISIBLE_DEVICES
 
 python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/unit_cell_graph_test/setup_json.json
+python test.py --test_data validation --setup_json ./models/unit_cell_graph_only_structure/setup_json.json
