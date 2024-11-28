fddca1eb64156cbf203111ded0c71975559e66a2
lgb543 <ufj@chem.ku.dk>
2024-11-28 16:31:08 +0100

    Experiment setup

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Central_atom_target_test
+#SBATCH --job-name=Central_atom_target
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=8000M
 
@@ -14,4 +14,4 @@ echo $CUDA_VISIBLE_DEVICES
 
 python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/Central_atom_target_test/setup_json.json
+python test.py --test_data validation --setup_json ./models/Central_atom_target/setup_json.json
