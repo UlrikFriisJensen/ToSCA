347c85c6d9a687b944405abac7bb0221bb9a1d09
lgb543 <ufj@chem.ku.dk>
2025-01-02 12:09:33 +0100

    Experiment setup

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=4d_split_ls_super_cell_unnormalizedCellParameters
+#SBATCH --job-name=6d_split_ls_super_cell_bigPrior
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=8000M
 
 #SBATCH -p gpu --gres=gpu:titanrtx:1
 
-#SBATCH --time=0-13:00:00
+#SBATCH --time=2-00:00:00
 
 #SBATCH -o ./slurm_outputs/scvae-%j.out #STDOUT
 
@@ -14,4 +14,4 @@ echo $CUDA_VISIBLE_DEVICES
 
 python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/4d_split_ls_super_cell_unnormalizedCellParameters/setup_json.json
+python test.py --test_data validation --setup_json ./models/6d_split_ls_super_cell_bigPrior/setup_json.json
