3a8673d36ca75c0e1d234353ddbe3a37c7328116
lgb543 <ufj@chem.ku.dk>
2025-01-17 16:04:19 +0100

    Experiment setup

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Combined_data_feedback_decoding
+#SBATCH --job-name=Combined_data_feedback_decoding_bigDecoder
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=8000M
 
@@ -14,4 +14,4 @@ echo $CUDA_VISIBLE_DEVICES
 
 python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/Combined_data_feedback_decoding/setup_json.json
+python test.py --test_data validation --setup_json ./models/Combined_data_feedback_decoding_bigDecoder/setup_json.json
