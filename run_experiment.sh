4b07a2caf39125a8f418ff9f633385e65f89fd7a
lgb543 <ufj@chem.ku.dk>
2025-03-19 14:24:06 +0100

    Test using interpolation data

--- run_experiment.sh
+++ run_experiment.sh
@@ -14,7 +14,7 @@ echo $CUDA_VISIBLE_DEVICES
 
 python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/Supercell_latentLog_beta_annealing_3d_latentMSE_biggerDecoder_v2/setup_json.json
+python test.py --test_data validation --setup_json ./models/Interpolation_Supercell_latentLog_beta_annealing_2d_latentMSE_biggerDecoder/setup_json.json
 
 # python test_experimentalData.py --data_folder ./data/Experimental/Jens/ --setup_json ./models/Combined_data_run_3d/setup_json.json
 # python test_interpolation.py --setup_json ./models/Combined_data_run_3d/setup_json.json
