2dee6bde27a4005e68550cc7b06af18b7a6aefcf
lgb543 <ufj@chem.ku.dk>
2025-03-21 15:23:39 +0100

    Experiment setup

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=Interpolation_Supercell_latentLog_beta_annealing_2d_latentMSE_biggerDecoder
+#SBATCH --job-name=Interpolation_Supercell_latentLog_beta_annealing_2d_latentMSE
 
 #SBATCH --ntasks=1 --cpus-per-task=12 --mem=8000M
 
-#SBATCH -p gpu --gres=gpu:titanrtx:1
+#SBATCH -p gpu --gres=gpu:1
 
 #SBATCH --time=2-00:00:00
 
@@ -14,7 +14,7 @@ echo $CUDA_VISIBLE_DEVICES
 
 python train.py --setup_json test_setup.json
 
-python test.py --test_data validation --setup_json ./models/Interpolation_Supercell_latentLog_beta_annealing_2d_latentMSE_biggerDecoder/setup_json.json
+python test.py --test_data validation --setup_json ./models/Interpolation_Supercell_latentLog_beta_annealing_2d_latentMSE/setup_json.json
 
 # python test_experimentalData.py --data_folder ./data/Experimental/Jens/ --setup_json ./models/Combined_data_run_3d/setup_json.json
 # python test_interpolation.py --setup_json ./models/Combined_data_run_3d/setup_json.json
